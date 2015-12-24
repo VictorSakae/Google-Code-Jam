3761bba093a618498a3a3eed4c0d38dd6ab377af
VictorSakae <sakaegushiken@gmail.com>
2015-12-23 22:34:36 -0200

    Add Store Credit problem

---
+++ Africa 2010/Qualification Round/Store Credit/StoreCredit.java
@@ -0,0 +1,16 @@
+/**
+ * @author VictorSakae
+ * Problem A. Store Credit
+ * https://code.google.com/codejam/contest/351101/dashboard
+ */
+public class StoreCredit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
