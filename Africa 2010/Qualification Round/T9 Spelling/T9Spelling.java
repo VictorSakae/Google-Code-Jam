fb8a69a9c2fdf5d137c5076200806cdec07457cc
VictorSakae <sakaegushiken@gmail.com>
2015-12-25 22:07:08 -0200

    Add T9 Spelling problem

---
+++ Africa 2010/Qualification Round/T9 Spelling/T9Spelling.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem C. T9 Spelling
+ * https://code.google.com/codejam/contest/351101/dashboard#s=p2
+ */
+
+public class T9Spelling {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
