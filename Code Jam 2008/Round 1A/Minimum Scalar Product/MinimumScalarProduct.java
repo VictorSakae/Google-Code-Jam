6b2e902d7e42577aaab045732d8bfa2be3e11169
VictorSakae <sakaegushiken@gmail.com>
2015-12-27 18:44:19 -0200

    Add Minimum Scalar Product problem

---
+++ Code Jam 2008/Round 1A/Minimum Scalar Product/MinimumScalarProduct.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. Minimum Scalar Product
+ * https://code.google.com/codejam/contest/32016/dashboard
+ */
+
+public class MinimumScalarProduct {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
