ac07d7f0190f42990589a2036e43212f57a52b91
VictorSakae <sakaegushiken@gmail.com>
2015-12-28 09:13:29 -0200

    Add Alien Language problem

---
+++ Code Jam 2009/Qualification Round/Alien Language/AlienLanguage.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. Alien Language
+ * https://code.google.com/codejam/contest/90101/dashboard#s=p0
+ */
+
+public class AlienLanguage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
