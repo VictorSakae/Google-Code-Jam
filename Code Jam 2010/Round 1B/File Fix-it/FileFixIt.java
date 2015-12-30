dd56a1a14356149786e37f0046cfe54d0891deb9
VictorSakae <sakaegushiken@gmail.com>
2015-12-30 08:00:15 -0200

    Add File Fix-it problem

---
+++ Code Jam 2010/Round 1B/File Fix-it/FileFixIt.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. File Fix-it
+ * https://code.google.com/codejam/contest/635101/dashboard
+ */
+
+public class FileFixIt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
