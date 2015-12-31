0d0a8e7f0e7a4c8c63c1098f0a6bd8abd03c8aa0
VictorSakae <sakaegushiken@gmail.com>
2015-12-31 10:52:35 -0200

    Add Odd Man Out problem

---
+++ Africa 2010/Online Competition/Odd Man Out/OddManOut.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. Odd Man Out
+ * https://code.google.com/codejam/contest/438101/dashboard
+ */
+
+public class OddManOut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
