20e36181cb4120fdf6b5d236a4aad0dee0b549a4
VictorSakae <sakaegushiken@gmail.com>
2015-12-31 16:16:23 -0200

    Add Text Messaging Outrage problem

---
+++ Code Jam 2008/Round 1C/Text Messaging Outrage/TextMessagingOutrage.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. Text Messaging Outrage
+ * https://code.google.com/codejam/contest/32015/dashboard
+ */
+
+public class TextMessagingOutrage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
