387e71007d5fe71873fc829ce0b21d7765c6861e
VictorSakae <sakaegushiken@gmail.com>
2016-01-03 21:41:08 -0200

    Add Rotate problem

---
+++ Code Jam 2010/Round 1A/Rotate/Rotate.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. Rotate
+ * https://code.google.com/codejam/contest/544101/dashboard
+ */
+
+public class Rotate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
