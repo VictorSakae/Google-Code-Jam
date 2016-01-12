c2d3e350526919445766cebf2c350fb3bfa3af85
VictorSakae <sakaegushiken@gmail.com>
2016-01-12 10:04:33 -0200

    Add All Your Base problem

---
+++ Code Jam 2009/Round 1C/All Your Base/AllYourBase.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem A. All Your Base
+ * https://code.google.com/codejam/contest/189252/dashboard
+ */
+
+public class AllYourBase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
