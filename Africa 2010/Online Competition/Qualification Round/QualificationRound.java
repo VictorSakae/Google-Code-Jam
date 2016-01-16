7e35fd77b710a71eb5375c4224749c2c645dec34
VictorSakae <sakaegushiken@gmail.com>
2016-01-15 23:48:26 -0200

    Add Qualification Round problem

---
+++ Africa 2010/Online Competition/Qualification Round/QualificationRound.java
@@ -0,0 +1,17 @@
+/**
+ * @author VictorSakae
+ * Problem C. Qualification Round
+ * https://code.google.com/codejam/contest/438101/dashboard#s=p2&a=2
+ */
+
+public class QualificationRound {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
